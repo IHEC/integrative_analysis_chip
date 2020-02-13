077a00bc0a38330a0da300d182fe1d14e4132078
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2020-02-13 16:53:58 -0500

    Path to cromwell fix inside template for piperunner

--- encode-wrapper/testrun_template.sh
+++ encode-wrapper/testrun_template.sh
@@ -13,5 +13,5 @@ BACKEND_CONF="{backend}"
 WORKFLOW_OPT="{container}"
 CHIP="{wdl}"
 
-java -jar -Dconfig.file=$BACKEND_CONF -Dbackend.default=$BACKEND $OUTDIR cromwell-34.jar run $CHIP -i $jobFile -o $WORKFLOW_OPT
+java -jar -Dconfig.file=$BACKEND_CONF -Dbackend.default=$BACKEND $OUTDIR $CROMWELL_HOME/cromwell-34.jar run $CHIP -i $jobFile -o $WORKFLOW_OPT
 echo "return:$?"
