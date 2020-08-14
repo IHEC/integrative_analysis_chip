8336919d1743b74e90a366ac7f1622f7c32ba25e
sitag <sitag@users.noreply.github.com>
2020-08-14 14:37:05 -0400

    Update bamstrip.scala

--- encode-wrapper/bamstrip/bamstrip.scala
+++ encode-wrapper/bamstrip/bamstrip.scala
@@ -8,6 +8,23 @@ import java.io._
 // cigar string is replaced by 1M, sequence by N, qualities by #
 // the output is written to stdout and can be converted back to bam
 
+/*
+SAM format specification: https://samtools.github.io/hts-specs/SAMv1.pdf
+
+Col Field Type Regexp/Range Brief description
+1 QNAME String [!-?A-~]{1,254} Query template NAME
+2 FLAG Int [0, 216 −1] bitwise FLAG
+3 RNAME String \*|[:rname:∧*=][:rname:]* Reference sequence NAME^11
+4 POS Int [0, 231 −1] 1-based leftmost mapping POSition
+5 MAPQ Int [0, 28 −1] MAPping Quality
+6 CIGAR String \*|([0-9]+[MIDNSHPX=])+ CIGAR string
+7 RNEXT String \*|=|[:rname:∧*=][:rname:]* Reference name of the mate/next read
+8 PNEXT Int [0, 231 −1] Position of the mate/next read
+9 TLEN Int [− 231 + 1, 231 −1] observed Template LENgth
+10 SEQ String \*|[A-Za-z=.]+ segment SEQuence
+11 QUAL String [!-~]+ ASCII of Phred-scaled base QUALity+
+*/
+
 object BamSeqStrip {
   def handleHeader(buffered:BufferedReader, out:BufferedWriter):(Int, String) = {
      var n:Int = 0
